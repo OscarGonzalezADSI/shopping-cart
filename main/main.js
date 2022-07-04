8c805b9d76e6da4e799df0aea68e1962810a30b4
OSCAR GONZALEZ <55475821+OscarGonzalezADSI@users.noreply.github.com>
2022-07-04 18:15:13 -0500

    Create main.js

---
+++ main/main.js
@@ -0,0 +1,29 @@
+class Main
+{
+
+  constructor()
+  {
+    this.controllerCarrito();
+  }
+  
+  controllerCarrito()
+  {
+    this.controllerCarrito = new ControllerCarrito();
+  }
+  
+}
+
+function controller_carrito()
+{
+  const main = new Main();
+}
+
+
+function main()
+{
+  controller_carrito();
+}
+
+
+const data = [];
+main();
