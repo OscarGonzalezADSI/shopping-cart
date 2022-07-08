450e1fe5bd6bb390ec4d5bf62e90c03950199e6e
OSCAR GONZALEZ <55475821+OscarGonzalezADSI@users.noreply.github.com>
2022-07-08 16:29:22 -0500

    clean code styles

--- main/main.js
+++ main/main.js
@@ -15,9 +15,65 @@ class Main
 
 function controller_carrito()
 {
-  const main = new Main();
+	new Main();
 }
 
+function moduleNone()
+{
+	document.getElementById('newsletter').style.display='none';
+	document.getElementById('newsletter2').style.display='none';
+	document.getElementById('newsletter3').style.display='none';
+}
+
+function imputsClean()
+{
+	document.getElementById('producto').value = "";
+	document.getElementById('precio').value = "";
+	document.getElementById('cantidad').value = "";
+}
+
+function moduleBlock(module)
+{
+	document.getElementById(module).style.display='block';
+}
+
+function itemBefore()
+{
+	moduleNone();
+}
+
+function itemNext()
+{
+	moduleNone();
+}
+
+function cancelBuy()
+{
+	moduleNone();
+}
+
+function payBill()
+{
+	moduleNone();
+}
+
+function openModuleBuy()
+{
+	moduleNone();
+	moduleBlock('newsletter');
+}
+
+function openModuleRequisition()
+{
+	moduleNone();
+	moduleBlock('newsletter2');
+}
+
+function openModuleFacture()
+{
+	moduleNone();
+	moduleBlock('newsletter3');
+}
 
 function main()
 {
@@ -26,6 +82,6 @@ function main()
   controller_carrito();
 }
 
-
 const data = [];
 main();
+
--- service/service.carrito.js
+++ service/service.carrito.js
@@ -1,15 +1,18 @@
 function service_carrito()
 {
-  controller_carrito();
+	moduleNone();
+	moduleBlock('newsletter2');
+    controller_carrito();
+	imputsClean();
 }
 
 function service_carrito_calcular(nodes)
 {
-  const precioValue = parseFloat(nodes.precio.value);
-  const cantidadValue = parseFloat(nodes.cantidad.value);
-  const res = precioValue * cantidadValue;
-  const resToString = (res).toString();
-  return resToString;
+	const precioValue = parseFloat(nodes.precio.value);
+	const cantidadValue = parseFloat(nodes.cantidad.value);
+	const res = precioValue * cantidadValue;
+	const resToString = (res).toString();
+	return resToString;
 }
 
 
